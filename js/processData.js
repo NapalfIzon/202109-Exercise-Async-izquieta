d7a1ae0f5bed8ed3d644b61f3e7f8b674ab3efc9
Napoleon Izquieta <napoleon.izquieta@gmail.com>
2021-10-13 12:48:19 +0200

    :memo:  add getUserDataById code in processData file

--- js/processData.js
+++ js/processData.js
@@ -3,7 +3,23 @@ function getUserId(name) {
     if (name.toLowerCase() === "luis") {
       return "I'm sick of you!";
     } else {
-      return Math.floor(Math.random() * 4815162342);
+      return Math.floor(Math.random(101));
+    }
+  }, 2000);
+}
+
+function getUserDataById(id) {
+  setTimeout((id) => {
+    if (id === 100) {
+      return "Luis, don't cheat on me!";
+    } else {
+      return {
+        name: "José Luis Torrente",
+        dni: "4815162342L",
+        age: "48 años",
+        city: "Madrid",
+        profession: "Apatrullador de la ciudad",
+      };
     }
   }, 2000);
 }
